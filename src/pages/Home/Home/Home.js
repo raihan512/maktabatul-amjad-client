6ad2dcc034330009a87fdeba6ab6f54ee8606e7d
raihan512 <raihangazi1024@gmail.com>
2023-03-03 22:53:26 +0600

    home down part added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Hero/Hero";
 import HomeBookTabs from "../HomeBookTabs/HomeBookTabs";
+import HomeRests from "../HomeRests/HomeRests";
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
       <HomeBookTabs></HomeBookTabs>
+      <HomeRests></HomeRests>
     </div>
   );
 };
